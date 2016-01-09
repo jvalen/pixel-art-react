58d9c11d273f46591bb1bd0592fcdcce1dc72223
jvalen <javiervalenciaromero@gmail.com>
2016-01-09 03:03:13 -0500

    Removed analytics from isomorphic render

--- app.js
+++ app.js
@@ -187,15 +187,6 @@ function renderFullPage(html, initialState) {
         <div id="app">${html}</div>
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
         <script src="bundle.js"></script>
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'UA-72177072-1', 'auto');
-          ga('send', 'pageview');
-        </script>
       </body>
     </html>
     `
