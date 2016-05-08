991261733b649e8fd3739a93c7dc157177050923
jvalen <javiervalenciaromero@gmail.com>
2016-05-08 02:57:45 +0200

    Pixel-cell component refactored to stateless component

--- src/components/Pixel-cell.jsx
+++ src/components/Pixel-cell.jsx
@@ -2,85 +2,80 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actionCreators from '../action_creators';
 
-export class PixelCell extends React.Component {
-  handleDrag(event) {
-    // If currently dragging
-    if (this.props.dragging) {
-      this.props.endDrag();
-      this.drawCell(event);
-      this.props.startDrag();
-    }
-  }
-
-  handleClick(event) {
-    this.drawCell(event);
-  }
-
-  handleMouseDown(event) {
-    this.drawCell(event);
-    this.props.startDrag();
-  }
-
-  drawCell() {
-    if (!this.props.eraserOn && !this.props.eyedropperOn) {
+const PixelCell = (props) => {
+  const drawCell = () => {
+    if (!props.eraserOn && !props.eyedropperOn) {
       // Apply the new color
-      this.props.setGridCellValue(
-        this.props.currentColor,
+      props.setGridCellValue(
+        props.currentColor,
         true,
-        this.props.id);
-    } else if (this.props.eraserOn) {
+        props.id
+      );
+    } else if (props.eraserOn) {
       // Color removed
-      this.props.setGridCellValue(
-        this.props.initialColor,
+      props.setGridCellValue(
+        props.initialColor,
         false,
-        this.props.id);
-    } else if (this.props.eyedropperOn) {
+        props.id);
+    } else if (props.eyedropperOn) {
       // Eyedropper
-      this.props.setColorSelected(this.props.color);
+      props.setColorSelected(props.color);
     }
+  };
+
+  const handleDrag = (event) => {
+    // If currently dragging
+    if (props.dragging) {
+      props.endDrag();
+      drawCell(event);
+      props.startDrag();
+    }
+  };
+
+  const handleMouseDown = (event) => {
+    drawCell(event);
+    props.startDrag();
+  };
+
+  const { color, width } = props;
+  const selectedColor = color;
+  let customCursor = 'cell';
+
+  if (props.eraserOn) {
+    customCursor = 'context-menu';
+  } else if (props.eyedropperOn) {
+    customCursor = 'copy';
   }
 
-  render() {
-    const { color, width } = this.props;
-    const selectedColor = color;
-    let customCursor = 'cell';
-
-    if (this.props.eraserOn) {
-      customCursor = 'context-menu';
-    } else if (this.props.eyedropperOn) {
-      customCursor = 'copy';
+  const styles = {
+    cellWrapper: {
+      display: 'inline-block',
+      width: `${width}%`,
+      boxSizing: 'border-box'
+    },
+    cell: {
+      backgroundColor: `#${selectedColor}`,
+      color: 'white',
+      position: 'relative',
+      width: '100%',
+      paddingBottom: '100%',
+      cursor: customCursor,
+      border: '1px solid #585858'
     }
+  };
 
-    const styles = {
-      cellWrapper: {
-        display: 'inline-block',
-        width: `${width}%`,
-        boxSizing: 'border-box'
-      },
-      cell: {
-        backgroundColor: `#${selectedColor}`,
-        color: 'white',
-        position: 'relative',
-        width: '100%',
-        paddingBottom: '100%',
-        cursor: customCursor,
-        border: '1px solid #585858'
-      }
-    };
-
-    return (
-      <div className="cellWrapper" style={styles.cellWrapper}>
-        <div
-          className="cell"
-          onMouseDown={(event) => { this.handleMouseDown(event); }}
-          onMouseUp={this.props.endDrag}
-          onMouseOver={(event) => { this.handleDrag(event); }}
-          style={styles.cell}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="cellWrapper" style={styles.cellWrapper}>
+      <div
+        className="cell"
+        onMouseDown={(event) => { handleMouseDown(event); }}
+        onMouseUp={props.endDrag}
+        onMouseOver={(event) => { handleDrag(event); }}
+        style={styles.cell}
+      />
+    </div>
+  );
+};
 
 function mapStateToProps(state) {
   return {
