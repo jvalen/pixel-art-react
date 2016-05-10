2772a92043a11a10a189924ed804fc1b9c033c74
jvalen <javiervalenciaromero@gmail.com>
2016-05-10 15:04:18 +0200

    Created component to add frames

---
+++ src/components/AddFrame.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../action_creators';
+
+const AddFrame = (props) => {
+  // TODO: Limit max frames (receive frames count)
+
+  const handleClick = () => {
+    props.createNewFrame();
+  };
+
+  return (
+    <button
+      className="add-frame gray"
+      onClick={() => { handleClick(); }}
+    >
+      New Frame
+    </button>
+  );
+};
+
+function mapStateToProps() {
+  return {};
+}
+export const AddFrameContainer = connect(
+  mapStateToProps,
+  actionCreators
+)(AddFrame);
