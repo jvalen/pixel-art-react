c4097d9065752fd24cc904731450d0f2a400b119
jvalen <javiervalenciaromero@gmail.com>
2016-05-27 18:30:11 +0200

    SimpleSpinner style integrated in postcss

--- src/components/SimpleSpinner.jsx
+++ src/components/SimpleSpinner.jsx
@@ -1,45 +1,9 @@
 import React from 'react';
 
 export const SimpleSpinner = (props) => {
-  const styles = {
-    wrapper: {
-      position: 'absolute',
-      zIndex: '1',
-      left: 0,
-      right: 0,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      display: props.spin ? 'block' : 'none',
-      width: '100%',
-      height: '100%',
-      backgroundColor: 'black',
-      opacity: '0.4',
-      top: 0
-    },
-    spin: {
-      height: '60px',
-      width: '60px',
-      margin: '94px auto 0 auto',
-      position: 'fixed',
-      WebkitAnimation: 'spin-rotation .6s infinite linear',
-      MozAnimation: 'spin-rotation .6s infinite linear',
-      OAnimation: 'spin-rotation .6s infinite linear',
-      animation: 'spin-rotation .6s infinite linear',
-      borderLeft: '6px solid rgba(239, 149, 50, 0.35)',
-      borderRight: '6px solid rgba(239, 149, 50, 0.35)',
-      borderBottom: '6px solid rgba(239, 149, 50, 0.35)',
-      borderTop: '6px solid rgba(219, 109, 26, 1)',
-      borderRadius: '100%',
-      top: '50%',
-      left: '50%',
-      marginTop: '-60px',
-      marginLeft: '-60px'
-    }
-  };
-
   return (
-    <div className="simple-spinner-wrapper" style={styles.wrapper}>
-      <div className="simple-spinner" style={styles.spin}></div>
+    <div className={`simple-spinner${props.spin ? 'display' : ''}`}>
+      <div className="circle"></div>
     </div>
   );
 };
