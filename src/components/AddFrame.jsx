3b5de44dbb4e25484ea65cc8a98e6820fc4fdda7
jvalen <javiervalenciaromero@gmail.com>
2016-05-28 19:52:46 +0200

    AddFrame style integrated in postcss

--- src/components/AddFrame.jsx
+++ src/components/AddFrame.jsx
@@ -3,15 +3,13 @@ import { connect } from 'react-redux';
 import * as actionCreators from '../action_creators';
 
 const AddFrame = (props) => {
-  // TODO: Limit max frames (receive frames count)
-
   const handleClick = () => {
     props.createNewFrame();
   };
 
   return (
     <button
-      className="add-frame gray"
+      className="add-frame"
       onClick={() => { handleClick(); }}
     >
       +
