5dc82a416edbff058f24f47b9b92c2f347f82dce
jvalen <javiervalenciaromero@gmail.com>
2016-06-03 02:40:43 +0200

    SimpleSpinner display bug fix

--- src/components/SimpleSpinner.jsx
+++ src/components/SimpleSpinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const SimpleSpinner = (props) => {
   return (
-    <div className={`simple-spinner${props.spin ? 'display' : ''}`}>
+    <div className={`simple-spinner${props.spin ? ' display' : ''}`}>
       <div className="circle"></div>
     </div>
   );
