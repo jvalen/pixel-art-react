84d0da57573f58c9b0c4cc934df6e183ffe87936
jvalen <javiervalenciaromero@gmail.com>
2016-06-10 00:44:23 +0200

    CellSize to stateless component

--- src/components/CellSize.jsx
+++ src/components/CellSize.jsx
@@ -3,29 +3,27 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actionCreators from '../store/actions/actionCreators';
 
-class CellSize extends React.Component {
-  handleCellSizeChange(event) {
-    this.props.actions.setCellSize(event.target.value | 0);
-  }
+const CellSize = (props) => {
+  const handleCellSizeChange = (event) => {
+    props.actions.setCellSize(event.target.value | 0);
+  };
 
-  render() {
-    const { cellSize } = this.props;
+  const { cellSize } = props;
 
-    return (
-      <div className="cell-size">
-        <label htmlFor="cell-size__input">
-          Pixel Size
-        </label>
-        <input
-          type="text"
-          value={cellSize}
-          onChange={(ev) => { this.handleCellSizeChange(ev); }}
-          id="cell-size__input"
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="cell-size">
+      <label htmlFor="cell-size__input">
+        Pixel Size
+      </label>
+      <input
+        type="text"
+        value={cellSize}
+        onChange={(ev) => { handleCellSizeChange(ev); }}
+        id="cell-size__input"
+      />
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   cellSize: state.present.get('cellSize')
