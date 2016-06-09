3c12353b5a991b05e6ac3d74de361cf9ace7d31f
jvalen <javiervalenciaromero@gmail.com>
2016-06-10 00:57:03 +0200

    DownloadDrawing to stateless component

--- src/components/DownloadDrawing.jsx
+++ src/components/DownloadDrawing.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import { shareDrawing } from '../utils/cssParse';
 
-export default class DownloadDrawing extends React.Component {
-  download(type) {
+const DownloadDrawing = (props) => {
+  const download = (type) => {
     const {
       frames, activeFrame, columns,
       rows, cellSize, duration
-    } = this.props;
+    } = props;
 
     shareDrawing(
       { type, frames, activeFrame, columns, rows, cellSize, duration },
       '',
       'download'
     );
-    this.props.actions.sendNotification('Downloading...');
-  }
+    props.actions.sendNotification('Downloading...');
+  };
 
-  render() {
-    return (
-      <button
-        className="download-drawing"
-        onClick={() => { this.download(this.props.downloadType); }}
-      >
-        DOWNLOAD
-      </button>
-    );
-  }
-}
+  return (
+    <button
+      className="download-drawing"
+      onClick={() => { download(props.downloadType); }}
+    >
+      DOWNLOAD
+    </button>
+  );
+};
+
+export default DownloadDrawing;
