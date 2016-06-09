4129967b91a7fab856c979b5823181860d1e08a7
jvalen <javiervalenciaromero@gmail.com>
2016-06-09 20:24:56 +0200

    SaveDrawing connect refactor

--- src/components/SaveDrawing.jsx
+++ src/components/SaveDrawing.jsx
@@ -1,40 +1,50 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as actionCreators from '../store/actions/actionCreators';
 import { saveProjectToStorage } from '../utils/storage';
 
-export class SaveDrawing extends React.Component {
-  save() {
+const SaveDrawing = (props) => {
+  const save = () => {
     const drawingToSave = {
-      frames: this.props.frames,
-      paletteGridData: this.props.paletteGridData,
-      cellSize: this.props.cellSize,
-      columns: this.props.columns,
-      rows: this.props.rows,
-      animate: this.props.frames.size > 1
+      frames: props.frames,
+      paletteGridData: props.paletteGridData,
+      cellSize: props.cellSize,
+      columns: props.columns,
+      rows: props.rows,
+      animate: props.frames.size > 1
     };
 
     if (saveProjectToStorage(localStorage, drawingToSave)) {
-      this.props.sendNotification('Drawing saved');
+      props.actions.sendNotification('Drawing saved');
     }
-  }
-  render() {
-    return (
-      <div className="save-drawing">
-        <button
-          onClick={() => { this.save(); }}
-        >
-          SAVE
-        </button>
-      </div>
-    );
-  }
-}
+  };
 
-function mapStateToProps() {
-  return {};
-}
-export const SaveDrawingContainer = connect(
+  return (
+    <div className="save-drawing">
+      <button
+        onClick={() => { save(); }}
+      >
+        SAVE
+      </button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  frames: state.present.get('frames'),
+  columns: state.present.get('columns'),
+  rows: state.present.get('rows'),
+  cellSize: state.present.get('cellSize'),
+  paletteGridData: state.present.get('paletteGridData')
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(actionCreators, dispatch)
+});
+
+const SaveDrawingContainer = connect(
   mapStateToProps,
-  actionCreators
+  mapDispatchToProps
 )(SaveDrawing);
+export default SaveDrawingContainer;
