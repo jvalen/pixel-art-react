a141a4da736f117334c709468b66bf0a0654a88a
jvalen <javiervalenciaromero@gmail.com>
2016-06-11 18:28:26 +0200

    json and urlencoded limit extended

--- src/server/server.js
+++ src/server/server.js
@@ -51,8 +51,8 @@ const oa = new OAuth(
 app.set('views', `${__dirname}/../views`);
 app.set('view engine', 'pug');
 app.use(express.static(`${__dirname}/../../deploy`));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(session(
   {
