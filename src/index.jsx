72e64fcecc7dbe96d841a06a3878af108171b6c5
jvalen <javiervalenciaromero@gmail.com>
2016-06-14 14:33:03 +0200

    Removed Perf library from production

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/imports.css'; // Import PostCSS files
-import Perf from 'react-addons-perf';
 import configureStore from './store/configureStore';
 import Root from './components/Root';
 
 const devMode = process.env.NODE_ENV === 'development';
 if (devMode) {
+  const Perf = require('react-addons-perf');
   window.Perf = Perf; // Expose react-addons-perf for dev purposes
 }
 
