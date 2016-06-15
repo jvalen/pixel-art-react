060e81aad6d0299eaff6c4342c5d006caf9f06bb
jvalen <javiervalenciaromero@gmail.com>
2016-06-15 13:49:01 +0200

    Add an example project loaded by default and it is included an exported data file

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,3 +1,4 @@
+import { exampleEyes } from '../../examples/example-eyes/json-eyes';
 const STORAGE_KEY = 'pixelart-react-v2';
 
 /*
@@ -28,8 +29,8 @@ function saveDataToStorage(storage, data) {
 */
 export function initStorage(storage) {
   storage.setItem(STORAGE_KEY, JSON.stringify({
-    stored: [],
-    current: -1
+    stored: [exampleEyes], // Load an example project data by default
+    current: 0
   }));
 }
 
