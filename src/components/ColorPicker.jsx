c032447d120f2deaf237038cdf8ecd832bb16014
jvalen <javiervalenciaromero@gmail.com>
2019-07-15 00:58:43 +0200

    Refactor ColorPicker component using hooks

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,109 +1,92 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Picker from 'react-color';
 import { switchTool, setCustomColor } from '../store/actions/actionCreators';
 import { COLOR_PICKER, PENCIL } from '../store/reducers/drawingToolStates';
 
-class ColorPicker extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayColorPicker: false
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-  }
+const ColorPickerContainer = React.memo(() => {
+  const [displayColorPicker, setDisplayColorPicker] = useState(false);
 
-  handleClick() {
-    const { switchColorPicker } = this.props;
-    const { displayColorPicker } = this.state;
+  const colorPickerOn = useSelector(
+    state => state.present.get('drawingTool') === COLOR_PICKER
+  );
+  const paletteColor = useSelector(state => {
+    const palette = state.present.get('palette');
+    const currentColorPosition = palette.get('position');
+    return palette.getIn(['grid', currentColorPosition, 'color']);
+  });
+
+  const dispatch = useDispatch();
+  const switchColorPickerAction = switchTool(COLOR_PICKER);
+  const setPencilToolAction = switchTool(PENCIL);
+
+  const isSelected = colorPickerOn && displayColorPicker;
+  const initialPickerColor = paletteColor || '#ffffff';
+
+  const handleClick = () => {
+    const switchColorPicker = () => dispatch(switchColorPickerAction);
     switchColorPicker();
     if (!displayColorPicker) {
-      this.setState({ displayColorPicker: !displayColorPicker });
+      setDisplayColorPicker(!displayColorPicker);
     }
-  }
-
-  handleClose() {
-    const { setPencilTool } = this.props;
-    this.setState({ displayColorPicker: false });
-    setPencilTool();
-  }
-
-  render() {
-    /* Necessary inline styles for react-color component */
-    const styles = {
-      picker: {
-        position: 'relative',
-        bottom: '9em'
-      },
-      popover: {
-        position: 'absolute',
-        zIndex: '2',
-        right: -250,
-        top: 155
-      },
-      cover: {
-        position: 'fixed',
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0
-      }
-    };
-    const { props, state } = this;
-    const { colorPickerOn, paletteColor } = props;
-    const { displayColorPicker } = state;
-    const isSelected = colorPickerOn && displayColorPicker;
-    const initialPickerColor = paletteColor || '#ffffff';
-
-    return (
-      <div className="color-picker">
-        <button
-          type="button"
-          className={`color-picker__button${isSelected ? ' selected' : ''}`}
-          onClick={this.handleClick}
-        />
-        <div style={styles.picker}>
-          {displayColorPicker ? (
-            <div style={styles.popover} is="popover">
-              <div
-                style={styles.cover}
-                is="cover"
-                onClick={this.handleClose}
-                role="presentation"
-              />
-              <Picker
-                color={initialPickerColor}
-                onChangeComplete={props.setCustomColor}
-                onClose={this.handleClose}
-                type="sketch"
-              />
-            </div>
-          ) : null}
-        </div>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  const palette = state.present.get('palette');
-  return {
-    colorPickerOn: state.present.get('drawingTool') === COLOR_PICKER,
-    paletteColor: palette.getIn(['grid', palette.get('position'), 'color'])
   };
-};
 
-const switchColorPickerAction = switchTool(COLOR_PICKER);
-const setPencilToolAction = switchTool(PENCIL);
-const mapDispatchToProps = dispatch => ({
-  switchColorPicker: () => dispatch(switchColorPickerAction),
-  setCustomColor: color => dispatch(setCustomColor(color.hex)),
-  setPencilTool: () => dispatch(setPencilToolAction)
+  const handleClose = () => {
+    setDisplayColorPicker(false);
+    const setPencilTool = () => dispatch(setPencilToolAction);
+    setPencilTool();
+  };
+
+  const onPickerChange = color => dispatch(setCustomColor(color.hex));
+
+  // Necessary inline styles for react-color component
+  const styles = {
+    picker: {
+      position: 'relative',
+      bottom: '9em'
+    },
+    popover: {
+      position: 'absolute',
+      zIndex: '2',
+      right: -250,
+      top: 155
+    },
+    cover: {
+      position: 'fixed',
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0
+    }
+  };
+
+  return (
+    <div className="color-picker">
+      <button
+        type="button"
+        className={`color-picker__button${isSelected ? ' selected' : ''}`}
+        onClick={handleClick}
+      />
+      <div style={styles.picker}>
+        {displayColorPicker ? (
+          <div style={styles.popover} is="popover">
+            <div
+              style={styles.cover}
+              is="cover"
+              onClick={handleClose}
+              role="presentation"
+            />
+            <Picker
+              color={initialPickerColor}
+              onChangeComplete={onPickerChange}
+              onClose={handleClose}
+              type="sketch"
+            />
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 });
 
-const ColorPickerContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ColorPicker);
 export default ColorPickerContainer;
