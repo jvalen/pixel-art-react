7769d473b5056cc6a642975544aac9ced5511b69
jvalen <javiervalenciaromero@gmail.com>
2020-01-19 14:01:20 +0100

    Add MOVE_DRAWING action tests

--- test/activeFrameReducer.test.js
+++ test/activeFrameReducer.test.js
@@ -3,27 +3,31 @@ import reducer from '../src/store/reducers/activeFrameReducer';
 import {
   APPLY_PENCIL,
   APPLY_ERASER,
-  APPLY_BUCKET
+  APPLY_BUCKET,
+  MOVE_DRAWING
 } from '../src/store/actions/actionTypes';
 import * as actions from '../src/store/actions/actionCreators';
 
-const firstGridMock = [
+const gridMock = [
   'rgba(17, 17, 17, 1)',
   'rgba(17, 17, 17, 1)',
   'rgba(34, 34, 34, 1)',
   'rgba(34, 34, 34, 1)',
   'rgba(34, 34, 34, 1)',
-  'rgba(51, 51, 51, 1)'
+  'rgba(51, 51, 51, 1)',
+  'rgba(12, 12, 12, 1)',
+  'rgba(12, 12, 12, 1)',
+  'rgba(12, 12, 12, 1)'
 ];
-const framesMock = () =>
+const framesMock = grid =>
   Map({
     list: List([
       Map({
-        grid: List(firstGridMock)
+        grid: List(grid)
       })
     ]),
     activeIndex: 0,
-    columns: 2,
+    columns: 3,
     rows: 3
   });
 
@@ -32,7 +36,7 @@ describe('framesReducer: APPLY_PENCIL', () => {
   const type = APPLY_PENCIL;
 
   it('draws cell with the palette color', () => {
-    const state = framesMock();
+    const state = framesMock(gridMock);
     const nextState = reducer(state, {
       type,
       paletteColor,
@@ -45,7 +49,10 @@ describe('framesReducer: APPLY_PENCIL', () => {
       'rgba(34, 34, 34, 1)',
       'rgba(34, 34, 34, 1)',
       'rgba(34, 34, 34, 1)',
-      'rgba(51, 51, 51, 1)'
+      'rgba(51, 51, 51, 1)',
+      'rgba(12, 12, 12, 1)',
+      'rgba(12, 12, 12, 1)',
+      'rgba(12, 12, 12, 1)'
     ]);
   });
 });
@@ -54,7 +61,7 @@ describe('framesReducer: APPLY_ERASER', () => {
   const type = APPLY_ERASER;
 
   it('draws cell with the palette color', () => {
-    const state = framesMock();
+    const state = framesMock(gridMock);
     const nextState = reducer(state, {
       type,
       id: 3
@@ -66,7 +73,10 @@ describe('framesReducer: APPLY_ERASER', () => {
       'rgba(34, 34, 34, 1)',
       '',
       'rgba(34, 34, 34, 1)',
-      'rgba(51, 51, 51, 1)'
+      'rgba(51, 51, 51, 1)',
+      'rgba(12, 12, 12, 1)',
+      'rgba(12, 12, 12, 1)',
+      'rgba(12, 12, 12, 1)'
     ]);
   });
 });
@@ -78,7 +88,7 @@ describe('framesReducer: APPLY_BUCKET', () => {
   const rows = 3;
 
   it('drawing color area with two cells', () => {
-    const state = framesMock();
+    const state = framesMock(gridMock);
     const nextState = reducer(state, {
       type,
       paletteColor,
@@ -93,12 +103,15 @@ describe('framesReducer: APPLY_BUCKET', () => {
       'rgba(34, 34, 34, 1)',
       'rgba(34, 34, 34, 1)',
       'rgba(34, 34, 34, 1)',
-      'rgba(51, 51, 51, 1)'
+      'rgba(51, 51, 51, 1)',
+      'rgba(12, 12, 12, 1)',
+      'rgba(12, 12, 12, 1)',
+      'rgba(12, 12, 12, 1)'
     ]);
   });
 
   it('drawing color area with three cells', () => {
-    const state = framesMock();
+    const state = framesMock(gridMock);
     const nextState = reducer(state, {
       type,
       paletteColor,
@@ -113,12 +126,15 @@ describe('framesReducer: APPLY_BUCKET', () => {
       paletteColor,
       paletteColor,
       paletteColor,
-      'rgba(51, 51, 51, 1)'
+      'rgba(51, 51, 51, 1)',
+      'rgba(12, 12, 12, 1)',
+      'rgba(12, 12, 12, 1)',
+      'rgba(12, 12, 12, 1)'
     ]);
   });
 
   it('drawing color area with one cell', () => {
-    const state = framesMock();
+    const state = framesMock(gridMock);
     const nextState = reducer(state, {
       type,
       paletteColor,
@@ -133,17 +149,23 @@ describe('framesReducer: APPLY_BUCKET', () => {
       'rgba(34, 34, 34, 1)',
       'rgba(34, 34, 34, 1)',
       'rgba(34, 34, 34, 1)',
-      paletteColor
+      paletteColor,
+      'rgba(12, 12, 12, 1)',
+      'rgba(12, 12, 12, 1)',
+      'rgba(12, 12, 12, 1)'
     ]);
   });
 });
 
 describe('framesReducer: SET_RESET_GRID', () => {
   it('resets the grid', () => {
-    const state = framesMock();
+    const state = framesMock(gridMock);
     const nextState = reducer(state, actions.resetGrid());
 
     expect(nextState.getIn(['list', 0, 'grid']).toJS()).toEqual([
+      '',
+      '',
+      '',
       '',
       '',
       '',
@@ -167,5 +189,102 @@ describe('framesReducer: CHANGE_FRAME_INTERVAL', () => {
     expect(nextState.getIn(['list', activeIndex, 'interval'])).toEqual(
       interval
     );
+  });
+});
+
+describe('framesReducer: MOVE_DRAWING', () => {
+  let grid;
+  let state;
+  let type;
+  beforeEach(() => {
+    grid = [
+      'rgba(1, 1, 1, 1)',
+      'rgba(2, 2, 2, 1)',
+      'rgba(3, 3, 3, 1)',
+      'rgba(4, 4, 4, 1)',
+      'rgba(5, 5, 5, 1)',
+      'rgba(6, 6, 6, 1)',
+      'rgba(7, 7, 7, 1)',
+      'rgba(8, 8, 8, 1)',
+      'rgba(9, 9, 9, 1)'
+    ];
+    state = framesMock(grid);
+    type = MOVE_DRAWING;
+  });
+  it('moves left the current frame drawing', () => {
+    const nextState = reducer(state, {
+      type,
+      moveDiff: { xDiff: -2, yDiff: 0, cellWidth: 1 }
+    });
+    const shiftedGrid = [
+      'rgba(2, 2, 2, 1)',
+      'rgba(3, 3, 3, 1)',
+      'rgba(1, 1, 1, 1)',
+      'rgba(5, 5, 5, 1)',
+      'rgba(6, 6, 6, 1)',
+      'rgba(4, 4, 4, 1)',
+      'rgba(8, 8, 8, 1)',
+      'rgba(9, 9, 9, 1)',
+      'rgba(7, 7, 7, 1)'
+    ];
+
+    expect(nextState.getIn(['list', 0, 'grid']).toJS()).toEqual(shiftedGrid);
+  });
+  it('moves right the current frame drawing', () => {
+    const nextState = reducer(state, {
+      type,
+      moveDiff: { xDiff: 2, yDiff: 0, cellWidth: 1 }
+    });
+    const shiftedGrid = [
+      'rgba(3, 3, 3, 1)',
+      'rgba(1, 1, 1, 1)',
+      'rgba(2, 2, 2, 1)',
+      'rgba(6, 6, 6, 1)',
+      'rgba(4, 4, 4, 1)',
+      'rgba(5, 5, 5, 1)',
+      'rgba(9, 9, 9, 1)',
+      'rgba(7, 7, 7, 1)',
+      'rgba(8, 8, 8, 1)'
+    ];
+
+    expect(nextState.getIn(['list', 0, 'grid']).toJS()).toEqual(shiftedGrid);
+  });
+  it('moves up the current frame drawing', () => {
+    const nextState = reducer(state, {
+      type,
+      moveDiff: { xDiff: 0, yDiff: -2, cellWidth: 1 }
+    });
+    const shiftedGrid = [
+      'rgba(4, 4, 4, 1)',
+      'rgba(5, 5, 5, 1)',
+      'rgba(6, 6, 6, 1)',
+      'rgba(7, 7, 7, 1)',
+      'rgba(8, 8, 8, 1)',
+      'rgba(9, 9, 9, 1)',
+      'rgba(1, 1, 1, 1)',
+      'rgba(2, 2, 2, 1)',
+      'rgba(3, 3, 3, 1)'
+    ];
+
+    expect(nextState.getIn(['list', 0, 'grid']).toJS()).toEqual(shiftedGrid);
+  });
+  it('moves down the current frame drawing', () => {
+    const nextState = reducer(state, {
+      type,
+      moveDiff: { xDiff: 0, yDiff: 2, cellWidth: 1 }
+    });
+    const shiftedGrid = [
+      'rgba(7, 7, 7, 1)',
+      'rgba(8, 8, 8, 1)',
+      'rgba(9, 9, 9, 1)',
+      'rgba(1, 1, 1, 1)',
+      'rgba(2, 2, 2, 1)',
+      'rgba(3, 3, 3, 1)',
+      'rgba(4, 4, 4, 1)',
+      'rgba(5, 5, 5, 1)',
+      'rgba(6, 6, 6, 1)'
+    ];
+
+    expect(nextState.getIn(['list', 0, 'grid']).toJS()).toEqual(shiftedGrid);
   });
 });
