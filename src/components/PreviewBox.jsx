1d748314e97b622a5b43d6b2b1f964b49d000876
jvalen <javiervalenciaromero@gmail.com>
2020-09-29 14:37:15 +0200

    Refactor Preview box to use Hooks

--- src/components/PreviewBox.jsx
+++ src/components/PreviewBox.jsx
@@ -1,37 +1,23 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as actionCreators from '../store/actions/actionCreators';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Preview from './Preview';
 
-class PreviewC extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      animate: false,
-      normalSize: true
-    };
+const PreviewBox = props => {
+  const [animate, setAnimate] = useState(false);
+  const [normalSize, setNormalSize] = useState(true);
+  const frames = useSelector(state => state.present.get('frames'));
+  const duration = useSelector(state => state.present.get('duration'));
+  const frameList = frames.get('list');
+  const activeFrameIndex = frames.get('activeIndex');
+  const columns = frames.get('columns');
+  const rows = frames.get('rows');
+  const { helpOn, callback } = props;
+  const animMessage = `${animate ? 'Pause' : 'Play'} the animation`;
+  const zoomMessage = `Zoom ${normalSize ? '0.5' : '1.5'}`;
+  const animTooltip = helpOn ? animMessage : null;
+  const zoomTooltip = helpOn ? zoomMessage : null;
 
-    this.togglePlay = this.togglePlay.bind(this);
-    this.toggleScreen = this.toggleScreen.bind(this);
-  }
-
-  togglePlay(e) {
-    const { animate } = this.state;
-    if (!animate) {
-      e.target.classList.remove('play');
-      e.target.classList.add('pause');
-    } else {
-      e.target.classList.remove('pause');
-      e.target.classList.add('play');
-    }
-    this.setState({
-      animate: !animate
-    });
-  }
-
-  toggleScreen(e) {
-    const { normalSize } = this.state;
+  const toggleScreen = e => {
     const box = document.querySelector('.box-container');
     const preview = document.querySelector('.preview-container');
 
@@ -46,86 +32,50 @@ class PreviewC extends React.Component {
       box.style.height = '';
       preview.style.height = '';
     }
-    this.setState({
-      normalSize: !normalSize
-    });
-  }
+    setNormalSize(!normalSize);
+  };
 
-  render() {
-    const { helpOn } = this.props;
-    const props = Object.assign(this.props);
-    const { animate, normalSize } = this.state;
-    let animTooltip = null;
-    let zoomTooltip = null;
-    if (helpOn) {
-      if (animate) animTooltip = 'Pause the animation';
-      else animTooltip = 'Play the animation';
-      if (normalSize) zoomTooltip = 'Zoom 0.5';
-      else zoomTooltip = 'Zoom 1.0';
-    }
-
-    return (
-      <div className="box-container">
-        <div className="buttons">
-          <div data-tooltip={animTooltip}>
-            <button
-              type="button"
-              className="play"
-              onClick={this.togglePlay}
-              aria-label="Animation control"
-            />
-          </div>
-          <div data-tooltip={zoomTooltip}>
-            <button
-              type="button"
-              className="screen-normal"
-              aria-label="Zoom button"
-              onClick={this.toggleScreen}
-            />
-          </div>
-          <div data-tooltip={helpOn ? 'Show a preview of your project' : null}>
-            <button
-              type="button"
-              className="frames"
-              aria-label="Active modal"
-              onClick={props.callback}
-            />
-          </div>
+  return (
+    <div className="box-container">
+      <div className="buttons">
+        <div data-tooltip={animTooltip}>
+          <button
+            type="button"
+            className={animate ? 'pause' : 'play'}
+            onClick={() => setAnimate(!animate)}
+            aria-label="Animation control"
+          />
         </div>
-        <div className="preview-container">
-          <Preview
-            key="1"
-            frames={props.frames}
-            columns={props.columns}
-            rows={props.rows}
-            cellSize={normalSize ? 6 : 3}
-            duration={props.duration}
-            activeFrameIndex={props.activeFrameIndex}
-            animate={animate}
+        <div data-tooltip={zoomTooltip}>
+          <button
+            type="button"
+            className="screen-normal"
+            aria-label="Zoom button"
+            onClick={toggleScreen}
+          />
+        </div>
+        <div data-tooltip={helpOn ? 'Show a preview of your project' : null}>
+          <button
+            type="button"
+            className="frames"
+            aria-label="Active modal"
+            onClick={callback}
           />
         </div>
       </div>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  const frames = state.present.get('frames');
-  const activeFrameIndex = frames.get('activeIndex');
-  return {
-    frames: frames.get('list'),
-    activeFrameIndex,
-    activeFrame: frames.getIn(['list', activeFrameIndex]),
-    paletteGridData: state.present.getIn(['palette', 'grid']),
-    columns: frames.get('columns'),
-    rows: frames.get('rows'),
-    duration: state.present.get('duration')
-  };
+      <div className="preview-container">
+        <Preview
+          frames={frameList}
+          columns={columns}
+          rows={rows}
+          cellSize={normalSize ? 6 : 3}
+          duration={duration}
+          activeFrameIndex={activeFrameIndex}
+          animate={animate}
+        />
+      </div>
+    </div>
+  );
 };
 
-const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators(actionCreators, dispatch)
-});
-
-const PreviewBox = connect(mapStateToProps, mapDispatchToProps)(PreviewC);
 export default PreviewBox;
