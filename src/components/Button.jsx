437eb3176f5b9bdc038a4616db149c8d7b6575a5
jvalen <javiervalenciaromero@gmail.com>
2021-07-24 03:43:06 +0200

    Load image from file improved layout
    Created a Button component that will replace old buttons
    in future iterations. Created to style de input file.

---
+++ src/components/Button.jsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { ThemeProvider, css } from 'styled-components';
+import theme from 'styled-theming';
+
+const colors = {
+  silver: '#bbb',
+  mineShaft: '#313131',
+  doveGray: '#707070',
+  tundora: '#4b4949',
+  lotus: '#803c3c',
+  buccaneer: '#733939',
+  cowboy: '#552a2a',
+  steelblue: '#5786c1',
+  sanMarino: '#4171ae',
+  eastBay: '#3a587f',
+  chathamsBlue: '#164075',
+  chambray: '#2f5382',
+  cloudBurst: '#253c5a',
+  alto: '#e0e0e0',
+  silveChalice: '#a0a0a0',
+  nobel: '#b7b7b7'
+};
+
+const textColor = theme.variants('mode', 'variant', {
+  default: { default: colors.silver },
+  action: { default: colors.silver },
+  close: { default: colors.silver },
+  info: { default: colors.silver },
+  white: { default: 'black' }
+});
+
+const bgColor = theme.variants('mode', 'variant', {
+  default: { default: colors.mineShaft },
+  info: { default: colors.lotus },
+  close: { default: colors.steelblue },
+  action: { default: colors.chathamsBlue },
+  white: { default: colors.alto }
+});
+
+const boxShadowColor = theme.variants('mode', 'variant', {
+  default: { default: colors.doveGray },
+  info: { default: colors.buccaneer },
+  close: { default: colors.sanMarino },
+  action: { default: colors.chambray },
+  white: { default: colors.silveChalice }
+});
+
+const bgActiveColor = theme.variants('mode', 'variant', {
+  default: { default: colors.tundora },
+  info: { default: colors.cowboy },
+  close: { default: colors.eastBay },
+  action: { default: colors.cloudBurst },
+  white: { default: colors.nobel }
+});
+
+const ButtonCSS = css`
+  background: none;
+  border: none;
+  outline: none;
+  border-radius: 2px;
+  padding: 10px;
+  font-size: 1em;
+  text-decoration: none;
+  transition-duration: 0.1s;
+  color: ${textColor};
+  background-color: ${bgColor};
+  box-shadow: 0 5px 0 0 ${boxShadowColor};
+  &:hover,
+  &.selected {
+    background-color: ${bgActiveColor};
+  }
+  &:hover {
+    cursor: pointer;
+  }
+  &:active {
+    transform: translate(0, 5px);
+    box-shadow: 0 1px 0 ${bgActiveColor};
+    background-color: ${bgActiveColor};
+  }
+`;
+
+const ButtonStyled = styled.button`
+  ${ButtonCSS}
+`;
+
+const InputFileLabelStyled = styled.label.attrs({ for: 'load-image-input' })`
+  ${ButtonCSS}
+  cursor: pointer;
+`;
+
+const InputFileStyled = styled.input.attrs({
+  type: 'file',
+  id: 'load-image-input'
+})`
+  opacity: 0;
+  position: absolute;
+  z-index: -1;
+`;
+
+const Button = ({ children, variant, onChange, type }) => (
+  <ThemeProvider theme={{ mode: 'default' }}>
+    {type === 'file' ? (
+      <>
+        <InputFileLabelStyled variant={variant}>
+          {children}
+        </InputFileLabelStyled>
+        <InputFileStyled onChange={onChange} />
+      </>
+    ) : (
+      <ButtonStyled variant={variant}>{children}</ButtonStyled>
+    )}
+  </ThemeProvider>
+);
+Button.propTypes = {
+  variant: PropTypes.oneOf(['default', 'info', 'close', 'action'])
+};
+
+Button.defaultProps = {
+  variant: 'default'
+};
+
+export default Button;
--- src/components/LoadImgFile.jsx
+++ src/components/LoadImgFile.jsx
@@ -1,10 +1,53 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { fromJS } from 'immutable';
 import shortid from 'shortid';
+import styled, { css } from 'styled-components';
 import getTimeInterval from '../utils/intervals';
+import Picker from './Picker';
+import LoadImgButton from './Button';
+
+const Container = styled.div`
+  text-align: center;
+  padding: 1rem 0;
+`;
+
+const Title = styled.h2`
+  display: block;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  text-align: center;
+`;
+
+const PropertiesContainer = styled.div`
+  display: none;
+  font-size: 1.5rem;
+  line-height: 2rem;
+
+  ${props =>
+    props.imageLoaded &&
+    css`
+      display: flex;
+      flex-wrap: wrap;
+    `}
+`;
+
+const LoadedImage = styled.div`
+  width: 50%;
+  padding: 0 1em;
+  background-color: #e2e2e2;
+`;
+
+const LoadSetup = styled.div`
+  width: 50%;
+  padding: 0 1em;
+`;
 
 const LoadImgFile = props => {
   const canvasRef = useRef(null);
+  const [frameCount, setFrameCount] = useState(1);
+  const [pixelSize, setPixelSize] = useState(1);
+  const [validationError, setValidationError] = useState('');
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -16,7 +59,7 @@ const LoadImgFile = props => {
 
   const onChange = ev => {
     const file = ev.target.files[0];
-    if (canvasRef && file.type.match('image.*')) {
+    if (canvasRef && file && file.type.match('image.*')) {
       const reader = new FileReader();
       const canvas = canvasRef.current;
       const context = canvas.getContext('2d');
@@ -27,6 +70,7 @@ const LoadImgFile = props => {
         context.canvas.width = img.width;
         context.canvas.height = img.height;
         context.drawImage(img, 0, 0);
+        setImageLoaded(true);
       };
       reader.readAsDataURL(file);
       reader.onload = function(evt) {
@@ -37,50 +81,59 @@ const LoadImgFile = props => {
     }
   };
 
-  const getHeightIntervals = (imageHeight, frameCount) => {
-    const heightPerFrame = Math.floor(imageHeight / frameCount);
+  const getHeightIntervals = (imageHeight, numberOfFrames) => {
+    const heightPerFrame = Math.floor(imageHeight / numberOfFrames);
     const intervals = [];
     let top = 0;
     let bottom = heightPerFrame;
-    for (let i = 0; i < frameCount; i++) {
+    for (let i = 0; i < numberOfFrames; i++) {
       intervals.push({
         top,
         bottom,
-        timePercentage: getTimeInterval(i, frameCount)
+        timePercentage: getTimeInterval(i, numberOfFrames)
       });
       top += heightPerFrame;
-      bottom = heightPerFrame;
+      bottom += heightPerFrame;
     }
     return intervals;
   };
 
-  const generateFrames = (imageContext, frameCount = 1) => {
+  const generateFrames = (imageContext, numberOfFrames, pixSize = 1) => {
     const { width, height } = imageContext.canvas;
-    const heightIntervals = getHeightIntervals(height, frameCount);
+    const heightIntervals = getHeightIntervals(height, numberOfFrames);
+
+    // TODO: Validation | Check exact height intervals (no loose pixels)
+    // TODO: Validation | Only higher than 50 height when multiples images
 
     const frameCollection = [];
 
     heightIntervals.forEach(heightInterval => {
-      const currentImage = imageContext.getImageData(
-        0,
-        heightInterval.top,
-        width,
-        heightInterval.bottom
-      ).data;
-      frameCollection.push({
-        grid: currentImage.reduce((acc, rgbaProperty, index) => {
-          const colorPosition = acc.length ? acc.length - 1 : 0;
-          let colorValue = acc.length ? acc[colorPosition] : '';
+      const pixelWidth = pixSize;
+      const pixelHeight = pixSize;
 
-          if (index === 0 || index % 4 === 0) {
-            colorValue = `rgba(${rgbaProperty},`;
-            acc.push(colorValue);
-          } else {
-            colorValue += `${rgbaProperty}${index % 4 === 3 ? ')' : ','}`;
-            acc[colorPosition] = colorValue;
-          }
-          return acc;
-        }, []),
+      console.log(heightInterval.top, heightInterval.bottom);
+
+      const grid = [];
+      for (
+        let y = heightInterval.top;
+        y + pixelHeight <= heightInterval.bottom;
+        y += pixelWidth
+      ) {
+        for (let x = 0; x + pixelWidth <= width; x += pixelWidth) {
+          const currentPixel = imageContext.getImageData(
+            x,
+            y,
+            pixelWidth,
+            pixelHeight
+          ).data;
+          grid.push(
+            `rgba(${currentPixel[0]},${currentPixel[1]},${currentPixel[2]},${currentPixel[3]})`
+          );
+        }
+      }
+
+      frameCollection.push({
+        grid,
         interval: heightInterval.timePercentage,
         key: shortid.generate()
       });
@@ -89,32 +142,103 @@ const LoadImgFile = props => {
     return fromJS(frameCollection);
   };
 
+  const loadImgValidation = (context, size, frameAmount) => {
+    const maxPixelsWidth = 100;
+    const maxPixelsHeight = 100;
+
+    const widthPixelsFit = context.width % size === 0;
+    const heightPixelsFit = context.height % size === 0;
+
+    const pixelsWidth = context.width / size;
+    const pixelsHeight = context.height / size / frameAmount;
+
+    setValidationError('');
+
+    if (!widthPixelsFit || !heightPixelsFit) {
+      setValidationError('No pixel size valid');
+      return false;
+    }
+
+    if (pixelsWidth > maxPixelsWidth || pixelsHeight > maxPixelsHeight) {
+      setValidationError(
+        `Error: Max width pixels: ${maxPixelsWidth} VS ${pixelsWidth} - Max height pixels: ${maxPixelsHeight} VS ${pixelsHeight}`
+      );
+      return false;
+    }
+
+    return true;
+  };
+
   const onClick = () => {
     const { actions } = props;
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
-    const defaultPixelSize = 5;
-    const frameCount = 1;
 
-    const frames = generateFrames(context, frameCount);
+    if (
+      loadImgValidation(
+        {
+          width: context.canvas.width,
+          height: context.canvas.height
+        },
+        pixelSize,
+        frameCount
+      )
+    ) {
+      const CanvasByPixelSize = {
+        width: context.canvas.width / pixelSize,
+        height: context.canvas.height / pixelSize
+      };
+      const frames = generateFrames(context, frameCount, pixelSize);
 
-    actions.setDrawing(
-      frames,
-      [],
-      defaultPixelSize,
-      context.canvas.width,
-      Math.floor(context.canvas.height / frameCount)
-    );
+      actions.setDrawing(
+        frames,
+        [],
+        pixelSize,
+        CanvasByPixelSize.width,
+        Math.floor(CanvasByPixelSize.height / frameCount)
+      );
+    }
   };
 
   return (
-    <>
-      <input type="file" onChange={onChange} />
-      <button type="button" onClick={onClick}>
-        Load from file
-      </button>
-      <canvas width="300" height="300" ref={canvasRef} />
-    </>
+    <Container>
+      <Title>Create a project from an image file</Title>
+      <LoadImgButton type="file" onChange={onChange}>
+        BROWSE...
+      </LoadImgButton>
+      <p>{validationError}</p>
+      <PropertiesContainer imageLoaded={imageLoaded}>
+        <LoadedImage>
+          <canvas
+            className="block mx-auto"
+            width="300"
+            height="300"
+            ref={canvasRef}
+          />
+        </LoadedImage>
+        <LoadSetup>
+          <h2>Number of Frames</h2>
+          <Picker
+            type="frame-count"
+            value={frameCount}
+            action={(type, behaviour) => {
+              setFrameCount(frameCount + behaviour);
+            }}
+          />
+          <h2>Pixel Size</h2>
+          <Picker
+            type="pixel-size"
+            value={pixelSize}
+            action={(type, behaviour) => {
+              setPixelSize(pixelSize + behaviour);
+            }}
+          />
+          <button type="button" onClick={onClick}>
+            Load from file
+          </button>
+        </LoadSetup>
+      </PropertiesContainer>
+    </Container>
   );
 };
 export default LoadImgFile;
