d01481a054fc263e0cc873258b1168d3421cc9d8
Dylan Mikus <dbmikus@gmail.com>
2021-11-14 13:48:18 -0800

    Don't load __INITIAL_STATE__ if not present
    When importing this as a library, `window.__INITIAL_STATE__` is not
    defined. If it's not defined, don't try to use it. This is some weird
    stuff that this codebase uses when you run it as a standalone
    application with a webserver instead of importing it.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,5 @@
 import { createStore } from 'redux';
 import undoable, { includeAction } from 'redux-undo';
-import { fromJS } from 'immutable';
 import reducer from './reducers/reducer';
 import {
   CHANGE_DIMENSIONS,
@@ -45,16 +44,12 @@ const configureStore = devMode => {
       type: SHOW_SPINNER
     });
   } else {
-    const initialState = window.__INITIAL_STATE__;
-    initialState.present = fromJS(initialState.present);
-
     store = createStore(
       undoable(reducer, {
         filter: createIncludedActions(),
         debug: false,
         ignoreInitialState: true
-      }),
-      initialState
+      })
     );
   }
 
