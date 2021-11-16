4145e539b89b6636f80f7ae425e4bda86aa8b2a5
Dylan Mikus <dbmikus@gmail.com>
2021-11-15 21:01:23 -0800

    Add an ethContext React context for dealing with Ethereum
    Add an ethContext React context and some scaffolding code such that the
    `PixelArt` component exposes some injection sites for functions that
    interact with the Ethereum chain. The `EthContextProvider` takes a
    function with the following signature:
    
    ```
    interface RGBA {
        // r, g, and b are integers between [0, 255]
        r: number,
        g: number,
        b: number,
    
        // a is a float in [0, 1]
        a: number
    }
    
    // grid is a 2D grid, so each inner array must be the same length
    (grid: Array<Array<RGBA>>): boolean
    ```
    
    The return value is whether minting succeeded or failed.

--- src/lib-index.jsx
+++ src/lib-index.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './css/imports.css'; // Import PostCSS files
 import configureStore from './store/configureStore';
+import { EthContextProvider } from './contexts/ethContext';
 import Root from './components/Root';
 
-export default function PixelArt() {
+export default function PixelArt({ mintFn }) {
   const [showEditor, setShowEditor] = useState(false);
   useEffect(() => {
     setShowEditor(true);
@@ -12,16 +13,20 @@ export default function PixelArt() {
   if (!showEditor) {
     return <div>Loading...</div>;
   }
-  return <PixelArtApp />;
+  return <PixelArtApp mintFn={mintFn} />;
 }
 
 // In case this code is pre-rendered by a server, it's simpler for us to have a
 // component that only gets rendered in the browser. Then we don't need to
 // worry about if all of the code is Node.js compatible because it can be gated
 // behind `useEffect`.
-function PixelArtApp() {
+function PixelArtApp({ mintFn }) {
   const devMode = process.env.NODE_ENV === 'development';
   const store = configureStore(devMode);
 
-  return <Root store={store} />;
+  return (
+    <EthContextProvider mintFn={mintFn}>
+      <Root store={store} />
+    </EthContextProvider>
+  );
 }
