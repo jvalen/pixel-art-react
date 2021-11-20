f217e443b3a1f181999369d9b9993af690af7c3e
Dylan Mikus <dbmikus@gmail.com>
2021-11-20 14:50:11 -0800

    Change default grid size to 24 cols, 30 rows

--- src/store/reducers/framesReducer.js
+++ src/store/reducers/framesReducer.js
@@ -73,8 +73,8 @@ const getFrame = (frames, frameId) => {
 
 const initFrames = (action = {}) => {
   const options = action.options || {};
-  const columns = parseInt(options.columns, 10) || 32;
-  const rows = parseInt(options.rows, 10) || 32;
+  const columns = parseInt(options.columns, 10) || 24;
+  const rows = parseInt(options.rows, 10) || 30;
   const list = resetIntervals(List([create(columns * rows)]));
   const hoveredIndex = undefined;
   return Map({
